3347d48c97e57e095d33075e19aefbf5549ecd5a
ᵈᵉᵛ | 𝒇𝒂𝒔𝒕𝒘𝒐𝒍𝒇❦ᵛᵃ <tempacc2816@gmail.com>
2026-09-15 18:38:50 +0300

    new improvements
    1) added utils module for beatuier logging
    2) done 9.2 lesson homework
    3) updated .gitignore

---
+++ src/widget.py
@@ -0,0 +1,19 @@
+import masks
+
+def mask_account_card(card_data: str) -> str:
+    card_number = card_data.split(" ")[-1] # take only the card number
+    card_type = card_data.rstrip(card_number)
+
+    if card_type.startswith("Счет"):
+        return card_type + masks.get_mask_account(card_number)
+    else:
+        return card_type + masks.get_mask_card_number(card_number)
+
+def get_date(date: str) -> str:
+    date_parts = date.split("-")
+
+    day = date_parts[2][0:2]
+    month = date_parts[1]
+    year = date_parts[0]
+
+    return f"{day}.{month}.{year}"
