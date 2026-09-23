01ab174b08854266ec7e697edca6266d68a5b885
ᵈᵉᵛ | 𝒇𝒂𝒔𝒕𝒘𝒐𝒍𝒇❦ᵛᵃ <tempacc2816@gmail.com>
2026-09-23 18:36:30 +0300

    fixed issues of 11.1

--- src/generators.py
+++ src/generators.py
@@ -1,13 +1,17 @@
 from collections.abc import Generator, Iterator
 from typing import Any
 
+
 def filter_by_currency(transactions: list[dict[str, Any]], currency: str) -> Iterator[dict[str, Any]]:
     """
     Функция принимает на вход список словарей (транзакции), и валюту по которой надо отфильтровать
     все транзакции клиента, вовзвращая в ответ итератор list[dict[str, Any].
     """
     # tx.get("значение", значение если элемента нет)
-    return (transaction for transaction in transactions if transaction.get("operation", {}).get("currency", {}).get("code") == currency)
+    return (transaction
+            for transaction in transactions
+            if transaction.get("operation", {}).get("currency", {}).get("code") == currency
+            )
 
 def transaction_descriptions(transactions: list[dict[str, Any]]) -> Generator[str, None, None]:
     """
@@ -32,6 +36,6 @@ def card_number_generator(start: int, stop: int) -> Iterator[str]:
     f"{card_number:016_}".replace("_", " ") преобразует числа в 16-значный десятичный формат
     с нулями в начале, если номер карты короткий.
     """
-    
+
     for card_number in range(start, stop + 1):
         yield f"{card_number:016_}".replace("_", " ")
